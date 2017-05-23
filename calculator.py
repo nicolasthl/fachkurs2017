67fd43317ba0df317a81a411c5f4c51028c8d5e3
nicolas liem <liemnico@hu-berlin.de>
2017-05-23 16:14:15 +0200

    Finishing day no.1 w/ Jesper Romers

--- calculator.py
+++ calculator.py
@@ -1,20 +1,67 @@
+from math import gcd
+#from math import lcm
+
+
 class Expression:
+	def evaluate(self):
+		pass
+
+class Operation(Expression):
+	def __init__(self, left_operand, right_operand):
+		assert isinstance(left_operand, Expression) and \
+		isinstance(right_operand, Expression)
+
+		self.left_operand = left_operand
+		self.right_operand = right_operand
 	pass
 
-class Operation(Expression)
-	pass
-class Sum(Operation)
-	pass
-class Diff(Operation)
-	pass
-class Multi(Operation)
-	pass
-class Div(Operation)
+class Sum(Operation):
+	def evaluate(self):
+		return self.left_operand.evaluate() + self.right_operand.evaluate()
+
+class Diff(Operation):
 	pass
 
-class Fraction(Expression)
+class Multi(Operation):
+	def evaluate(self):
+		return self.left_operand.evaluate() * self.right_operand.evaluate()
+
+class Div(Operation):
+	pass
+
+class Fraction(Expression):
 	def __init__(self,num,denom):
 		#what about "self"? => To adress the object itself.
+		if denom == 0:
+			raise ZeroDivisionError
 		self.num = num
 		self.denom = denom
+		self.simplify()
 	pass
+
+
+	def __eq__(self, other):
+		#underscore methods: pre-defined! also: 'magic' methods
+		return self.num == other.num and \
+		self.denom == other.denom 
+
+	def __mul__(self, other):
+		return self.product(other)
+	
+	def __add__(self,other):
+		return self.add(other)
+
+	def simplify(self):						#obsolete, as we perform already in constructor
+		divisor = gcd(self.num, self.denom)
+		self.num = self.num // divisor 		#souble slash for integer
+		self.denom = self.denom // divisor
+
+	def add(self, other):
+		return Fraction(self.num * other.denom + self.denom * other.num, self.denom * other.denom)
+
+	def product(self, other):
+		return Fraction(self.num * other.num, 
+						self.denom * other.denom) 
+
+	def evaluate(self):
+		return self
--- test_calculator.py
+++ test_calculator.py
@@ -1,14 +1,33 @@
-import calculator.py as calc
+#import calculator as calc
+from calculator import *
+
 
 #we will code a test calculator here
 def test_trivial_example():
 	assert 1 == 1
 	#assert means: test on a boolean condition, assign it's result.
 
-def test_fraction():
+def test_fraction_init():
+	frac = Fraction(3,4)
+	assert frac.num == 3
+	assert frac.denom == 4
 	pass
 
-
-
+#frac = calc.Fraction (3, 0)
 #this actually is a test funcition, declared by 'test_...' prefix
 #if we call "python3 -m 'pytest'" in a given directory, it will check for every python test funcitons.
+
+def test_fraction_multiply():
+	assert Fraction(1,2) * Fraction(2,4) == Fraction(1,4)
+
+def test_fraction_addition():
+	assert Fraction(1,2) + Fraction (1,4) == Fraction(3,4)
+
+def test_fraction_simplify():
+	assert Fraction(1,2) == Fraction(2,4)
+
+def test_fraction_equate():
+	pass
+
+def test_sum():
+	assert Sum(Fraction(1,2), Fraction(1, 4)).evaluate() == Fraction(3,4)
