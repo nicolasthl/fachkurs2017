9bf40cd6f1d24f097f79c8ab9b0d21835822581a
nicolas liem <liemnico@hu-berlin.de>
2017-06-01 18:47:22 +0200

    finished visualization as of thursday. added histograms

--- model.py
+++ model.py
@@ -1,9 +1,11 @@
-import database
-from molecules import Ribo, Protein, MRNA, PopulationCollection, ParticleCollection
-from translation import Translation
-import processes
 import numpy as np
-import matplotlib.pylab as plt
+from matplotlib import pyplot as plt
+import matplotlib as mpl
+import database
+from molecules import Ribo, Protein, MRNA, PopulationCollection, ParticleCollection, DNA, Polymerase
+from translation import Translation
+from transcription import Transcription
+import processes
 
 
 class SimulationResult:
@@ -25,26 +27,9 @@ class SimulationResult:
         @param time: float
         @return: None
         """
-        self.trace.append(self.molecule_collection.count()) 
+        self.trace.append(self.molecule_collection.count())
         self.time.append(time)
-        
-    def plot_directly(self):
-        plt.plot(self.time, self.trace)
-        print(self.trace)
-        plt.xlabel('some time ticks')
-        plt.ylabel('even more numbers')
-        plt.show()
-    #fehlt welche spezies
-    #fehlt design zeugs (farben, formen...)
-    
-    def to_file(self,fname=None):
-        a = np.copy(self.time)
-        b = np.concatenate(a,np.copy(self.trace))
-        if fname is None:
-            fname = 'defaultresult'
-        numpy.savetxt(fname+'.txt', b, delimiter=' ', newline='\n')
-        
-    #def plot_from_file(): #from local directory, fetch file. Plot it, given a plotting method.
+
 
 class Model:
     """
@@ -66,19 +51,33 @@ class Model:
 
     def _initialize_states(self):
         self.states[Ribo] = PopulationCollection(Ribo)
-        self.states[Ribo].populate("free ribos", 10)
+        self.states[Ribo].populate("free ribos", 50)
+        self.states[Polymerase] = PopulationCollection(Polymerase)
+        self.states[Polymerase].populate("Polymerase_total", 30) #4600 in natur
+        #self.states[Polymerase].molecules("Polymerase_bound") #updated each step
+        #self.model.states[Polymerase].molecules["Polymerase_total"]
+        #self.model.states[Polymerase].molecules["Polymerase_bound"]
+
+        self.states[DNA] = ParticleCollection(DNA)
+        self.states[DNA].add(DNA("DNA", self))
+
+
         self.states[MRNA] = ParticleCollection(MRNA)
-        for name, sequence in self.db.get_states(MRNA):
-            self.states[MRNA].add(MRNA(name, sequence))
+        #for name, sequence in self.db.get_states(MRNA):
+            #self.states[MRNA].add(MRNA(name, sequence))
         self.states[Protein] = ParticleCollection(Protein)
 
     def _initialize_processes(self):
         self.processes[Translation] = Translation("Translation", self)
+        self.processes[Transcription] = Transcription("Transcription", self)
 
     def step(self):
         """
         Do one update step for each process and save the results.
         """
+
+
+
         for p in self.processes:
             self.processes[p].update()
 
@@ -88,6 +87,7 @@ class Model:
         self.timestep += 1
 
     def simulate(self, steps, log=True):
+        POlist= []#states 0 or 1
         """
         Simulate the model for some time.
         @param steps: int
@@ -99,11 +99,34 @@ class Model:
             if log:  # This could be an entry point for further logging
                 print('mRNAs', self.states[MRNA].count())
                 print("Proteins", self.states[Protein].count())
+            #print(self.states[Polymerase].molecules["Polymerase_total"])
+            #print(self.states[Polymerase].molecules["Polymerase_bound"])
+            POlist.append([self.states[Polymerase].molecules["Polymerase_total"]-self.states[Polymerase].molecules["Polymerase_bound"],self.states[Polymerase].molecules["Polymerase_bound"]])
+            
+        self.plotListTraj(POlist, 50, 'Polymerasen: Bindungszustand über Zeit')
+            
+    def plotListTraj(self, somelist, ticks, title):
+        plotarr= np.asarray(somelist)
+        #now on to plotting
 
+        mpl.style.use('bmh')
+        fig = plt.figure(figsize=(25,10))
+        ax = fig.add_subplot(1,1,1)
+        ax.plot(plotarr[:,0], label='unbound', lw=3, c='r')
+        ax.plot(plotarr[:,1], label='bound', lw=3, c='b')
+        ax.set_title(title, fontsize=25)
+        ax.set_ylim(0,np.amax(plotarr))
+        ax.set_xlim(0,ticks)
+        ax.set_xlabel('time steps [sec]', fontsize=20)
+        ax.set_ylabel('count', fontsize=20)
+        ax.tick_params('both', labelsize=15)
+        ax.legend(loc=4, fontsize=25, frameon=False)
+        plt.show()
+        
+        
+    
 if __name__ == "__main__":
     c = Model()
-    c.simulate(100, log=True)
-    for state in c.results:              
-        c.results[state].plot_directly() #also für alle states -> simulationResult -> plot. Wollen wir das?
-        
-#trace = {'Clb5': [1,2,3,4,5,6], 'Cln2': [6,5,4,56,6,5,4]} # so sieht z.B. unsere trace aus. ein dictionary. schnallt matplotlib das ?
+    c.simulate(200, log=False)
+    #print(c.states[Protein].get_molecules())
+
